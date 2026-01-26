51e452d8b536ccc16df6f5c1216b5aa09d196590
AstroMallorca <astromca@gmail.com>
2026-01-26 19:42:49 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,34 @@
+const CACHE_NAME = "horitzo-v1";
+
 const CACHE_FILES = [
   "./",
   "./index.html",
-  "./manifest.webmanifest",
   "./style.css",
+  "./manifest.webmanifest",
   "./js/astronomy.browser.min.js",
-  "./data/horizon_profiles/perfil_demo.json"
+  "./data/horizon_profiles/perfil_demo.json",
+  "./assets/icons/icon-192.png",
+  "./assets/icons/icon-512.png"
 ];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CACHE_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
